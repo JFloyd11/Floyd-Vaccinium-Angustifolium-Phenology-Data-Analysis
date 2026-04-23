ce61115f5b8434338bc7d0bf9a688acb41694c06
TerrellRoulston <terrellroulston@hotmail.ca>
2026-04-23 11:28:35 -0300

    Path refactoring and added LRTs

--- 5 - Running The Models/EQ1_LMMs_Temp_Phenology.R
+++ 5 - Running The Models/EQ1_LMMs_Temp_Phenology.R
@@ -1,17 +1,15 @@
 #LMMs for temperature phenology relationships
 
-#Set working directory
-setwd("/Users/migicovskylab/Documents/Vaccinium Paper GitHub Files/5 - Running The Models")
-
 #Load packages
 library(dplyr)
 library(lme4)
 library(lmerTest)
+library(lmtest)
 
 #Import data and select important variables
-vaccinium <- read.csv("/Users/migicovskylab/Documents/Vaccinium Paper GitHub Files/5 - Running The Models/vaccinium_with_zones_weather_clean.csv")
+vaccinium <- read.csv("./5 - Running The Models/vaccinium_with_zones_weather_clean.csv")
 vaccinium <- vaccinium %>% 
-  select(!(X))
+  dplyr::select(!(X))
 
 #Separate data into phenology stages
 bud<- vaccinium %>% 
@@ -28,21 +26,37 @@ table(bud$month)
 bud_model <- lmer(jdate  ~ march_temperature + april_temperature + 
                     may_temperature + june_temperature + july_temperature + (1 | NA_L2NAME), data = bud)
 summary(bud_model)
+# Ecoregion random effect variance is negligible
+
+# LRT
+bud_lm <- lm(jdate ~ march_temperature + april_temperature + 
+               may_temperature + june_temperature + july_temperature, data = bud)
+
+lrtest(bud_lm, bud_model)
 
 #Flower model
 table(flower$month)
 flower_model <- lmer(jdate  ~ march_temperature + april_temperature + 
                        may_temperature + june_temperature + july_temperature + (1 | NA_L2NAME), data = flower)
 summary(flower_model)
+# Ecoregion random effect variance is negligible
+
+# LRT
+flower_lm <- lm(jdate ~ march_temperature + april_temperature + 
+                  may_temperature + june_temperature + july_temperature, data = flower)
+
+lrtest(flower_lm, flower_model)
 
 #Fruit model
 table(fruit$month)
 fruit_model <- lmer(jdate  ~ april_temperature + may_temperature + 
                       june_temperature + july_temperature + august_temperature + (1 | NA_L2NAME), data = fruit)
 summary(fruit_model)
+# Singular fit, remove the random effect
 
 #Seed disperse model
 table(sd$month)
 sd_model <- lmer(jdate  ~ may_temperature + june_temperature + july_temperature + 
                    august_temperature + sept_temperature + (1 | NA_L2NAME), data = sd)
 summary(sd_model)
+# Singular fit, remove the random effect
