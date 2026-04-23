b9cfa0d9e29992b9dd5dcc3d885d80e46a391d6b
TerrellRoulston <terrellroulston@hotmail.ca>
2026-04-23 11:27:35 -0300

    Path refactoring, and added 1.5 IQR outlier comparison

--- 2 - Removing Outliers/Removing_Outliers.R
+++ 2 - Removing Outliers/Removing_Outliers.R
@@ -1,7 +1,5 @@
 #Removing outliers
 
-#Set working directory
-setwd("/Users/migicovskylab/Documents/Vaccinium Paper GitHub Files/2 - Removing Outliers")
 
 #Load packages
 library(dplyr)
@@ -11,7 +9,7 @@ library(geodata)
 # Remove all specimens that are more than two standard deviations away from the mean
 #-------------------------------------------------------------------------------
 #Import dataset and select variables on interest
-vaccinium_with_jdates <- read.csv("initial_data.csv")
+vaccinium_with_jdates <- read.csv("./2 - Removing Outliers/initial_data.csv")
 vaccinium_with_jdates <- vaccinium_with_jdates %>% 
   select(!X)
 
@@ -93,6 +91,76 @@ merged_data_unique<-unique(merged_data)
 #the outlier removal rules for budding. 
 merged_data_unique<- merged_data_unique %>% 
   filter(!(reference %in% removed_unique$reference))
+
+#-------------------------------------------------------------------------------
+# IQR-based outlier detection (1.5 * IQR)
+#-------------------------------------------------------------------------------
+
+# Function to compute IQR limits
+get_iqr_limits <- function(x) {
+  q1 <- quantile(x, 0.25, na.rm = TRUE)
+  q3 <- quantile(x, 0.75, na.rm = TRUE)
+  iqr <- q3 - q1
+  lower <- q1 - 1.5 * iqr
+  upper <- q3 + 1.5 * iqr
+  return(list(lower = lower, upper = upper))
+}
+
+# Get limits for each phenophase
+bud_iqr <- get_iqr_limits(bud_data$jdate)
+flower_iqr <- get_iqr_limits(flower_data$jdate)
+fruit_iqr <- get_iqr_limits(fruit_data$jdate)
+sd_iqr <- get_iqr_limits(seed_disperse_data$jdate)
+vo_iqr <- get_iqr_limits(veg_only_data$jdate)
+
+# 1.5IQR filtering
+# Filter data using IQR
+iqr_bud <- bud_data %>%
+  filter(!(jdate < bud_iqr$lower | jdate > bud_iqr$upper))
+
+iqr_flower <- flower_data %>%
+  filter(!(jdate < flower_iqr$lower | jdate > flower_iqr$upper))
+
+iqr_fruit <- fruit_data %>%
+  filter(!(jdate < fruit_iqr$lower | jdate > fruit_iqr$upper))
+
+iqr_sd <- seed_disperse_data %>%
+  filter(!(jdate < sd_iqr$lower | jdate > sd_iqr$upper))
+
+iqr_vo <- veg_only_data %>%
+  filter(!(jdate < vo_iqr$lower | jdate > vo_iqr$upper))
+
+# Get removed observations
+# Removed by IQR
+removed_iqr <- rbind(
+  anti_join(bud_data, iqr_bud, by = "reference"),
+  anti_join(flower_data, iqr_flower, by = "reference"),
+  anti_join(fruit_data, iqr_fruit, by = "reference"),
+  anti_join(seed_disperse_data, iqr_sd, by = "reference"),
+  anti_join(veg_only_data, iqr_vo, by = "reference")
+) %>% unique()
+
+
+# -------------------------------------------------------------------------
+# Compare 2SD with 1.5 IQR
+# Already computed in your script:
+# removed_unique  (2SD removals)
+
+# Counts
+cat("2SD removed:", nrow(removed_unique), "\n")
+cat("IQR removed:", nrow(removed_iqr), "\n")
+
+# Overlap
+shared_removed <- intersect(removed_unique$reference, removed_iqr$reference)
+cat("Shared removed:", length(shared_removed), "\n")
+
+# Unique to each method
+only_2sd <- setdiff(removed_unique$reference, removed_iqr$reference)
+only_iqr <- setdiff(removed_iqr$reference, removed_unique$reference)
+
+cat("Only 2SD:", length(only_2sd), "\n")
+cat("Only IQR:", length(only_iqr), "\n")
+
 #-------------------------------------------------------------------------------
 # Remove veg only observations
 #-------------------------------------------------------------------------------
@@ -103,17 +171,17 @@ clean_data <- merged_data_unique %>%
 # Remove odd Quebec point
 #-------------------------------------------------------------------------------
 #Create a base map
-world<- world(path = "/Users/migicovskylab/Documents/Vaccinium Paper GitHub Files/2 - Removing Outliers")
+world<- world(path = "./gadm/")
 
 us_map <- gadm(country = 'USA', level = 1, resolution = 2,
-               path = "/Users/migicovskylab/Documents/Vaccinium Paper GitHub Files/2 - Removing Outliers") #USA basemap w. States
+               path = "./gadm/") #USA basemap w. States
 
 ca_map <- gadm(country = 'CA', level = 1, resolution = 2,
-               path = "/Users/migicovskylab/Documents/Vaccinium Paper GitHub Files/2 - Removing Outliers") #Canada basemap w. Provinces
+               path = "./gadm/") #Canada basemap w. Provinces
 
 canUS_map <- rbind(us_map, ca_map) #combine US and Canada vector map
 
-great_lakes<- vect("/Users/migicovskylab/Documents/Vaccinium Paper GitHub Files/2 - Removing Outliers/Great Lakes")
+great_lakes<- vect("./Great Lakes/")
 
 #Visualize the point in Northern Quebec on the out skirts of Hudson Bay
 plot(canUS_map, xlim = c(-96, -50), ylim = c(37, 60), background = 'lightblue', col = 'white', border = 'grey')
@@ -130,5 +198,17 @@ plot(great_lakes, add = TRUE, col = 'lightblue', border = 'grey')
 points(x = clean_data$decimalLongitude, y = clean_data$decimalLatitude, col = "red")
 
 #Save the clean dataset
-write.csv(clean_data, "/Users/migicovskylab/Documents/Vaccinium Paper GitHub Files/2 - Removing Outliers/clean_data.csv")
+write.csv(clean_data, "./2 - Removing Outliers/clean_data.csv")
 #-------------------------------------------------------------------------------
+# Summarizing initial data
+# -------------------------------------------------------------------------
+vaccinium_with_jdates %>% group_by(dataset) %>% count()
+
+vaccinium_with_jdates_EC <- vaccinium_with_jdates %>% filter(dataset == 'ECSmith')
+range(vaccinium_with_jdates_EC$year)
+
+vaccinium_with_jdates_herb <- vaccinium_with_jdates %>% filter(dataset == 'Online Herbarium')
+range(vaccinium_with_jdates_herb$year)
+
+vaccinium_with_jdates_inat <- vaccinium_with_jdates %>% filter(dataset == 'iNaturalist')
+range(vaccinium_with_jdates_inat$year)
