c02d029b429844aef206fbd7c3043f838b533c26
TerrellRoulston <terrellroulston@hotmail.ca>
2026-04-23 11:28:53 -0300

    path refactoring and added VIF tests

--- 5 - Running The Models/EQ2_LMs_Temp_Phenology.R
+++ 5 - Running The Models/EQ2_LMs_Temp_Phenology.R
@@ -1,15 +1,13 @@
 #LMs for temperature phenology models
 
-#Set working directory
-setwd("/Users/migicovskylab/Documents/Vaccinium Paper GitHub Files/5 - Running The Models")
-
 #Load packages
 library(dplyr)
+library(car)
 
 #Import data and select important variables 
-vaccinium <- read.csv("/Users/migicovskylab/Documents/Vaccinium Paper GitHub Files/5 - Running The Models/vaccinium_with_zones_weather_clean.csv")
+vaccinium <- read.csv("./5 - Running The Models/vaccinium_with_zones_weather_clean.csv")
 vaccinium <- vaccinium %>% 
-  select(!(X))
+  dplyr::select(!(X))
 
 #Separate data into phenology stages
 bud_data<- vaccinium %>% 
@@ -23,7 +21,7 @@ seed_disperse_data<- vaccinium %>%
 
 #Bud model
 bud_model2 <- lm(jdate  ~ march_temperature + april_temperature + may_temperature, data = bud_data)
-summary(bud_model2)
+summary(bud_model2, correlation = T)
 
 #Flower model
 fl_model2 <- lm(jdate  ~ march_temperature + april_temperature + may_temperature + july_temperature, data = flower_data)
@@ -31,10 +29,19 @@ summary(fl_model2)
 
 #Fruit model
 fr_model2 <- lm(jdate  ~ april_temperature + july_temperature, data = fruit_data)
-summary(fr_model2)
+summary(fr_model2, correlation = T)
 
 #Seed disperse model
 sd_model2 <- lm(jdate ~ may_temperature + june_temperature + august_temperature, 
                 data = seed_disperse_data)
-summary(sd_model2)
+summary(sd_model2, correlation = T)
+
+
+# -------------------------------------------------------------------------
+# Variable inflation check
+# -------------------------------------------------------------------------
+vif(bud_model2)
+vif(fl_model2)
+vif(fr_model2)
+vif(sd_model2)
 
