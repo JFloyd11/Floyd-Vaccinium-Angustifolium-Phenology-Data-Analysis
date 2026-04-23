86ca40223ca360c1a311b5f7dfb78403c9a43087
TerrellRoulston <terrellroulston@hotmail.ca>
2026-04-23 11:29:01 -0300

    Path refactoring and added LRTs

--- 5 - Running The Models/EQ3_LMMs_Time_Phenology.R
+++ 5 - Running The Models/EQ3_LMMs_Time_Phenology.R
@@ -1,15 +1,12 @@
 #LMMs for time phenology models
 
-#Set working directory
-setwd("/Users/migicovskylab/Documents/Vaccinium Paper GitHub Files/5 - Running The Models")
-
 #Load packages
 library(dplyr)
 library(lme4)
 library(lmerTest)
 
 #Import data and select import variables 
-vaccinium <- read.csv("/Users/migicovskylab/Documents/Vaccinium Paper GitHub Files/5 - Running The Models/vaccinium_with_zones_weather_clean.csv")
+vaccinium <- read.csv("./5 - Running The Models/vaccinium_with_zones_weather_clean.csv")
 vaccinium <- vaccinium %>% 
   select(!(X))
 
@@ -38,4 +35,21 @@ summary(fruit_model)
 #Seed disperse model
 sd_model <- lmer(jdate  ~ year + (1 | NA_L2NAME), data = sd)
 summary(sd_model)
+
+
+# -------------------------------------------------------------------------
+# LRTs
+# -------------------------------------------------------------------------
+# Bud
+bud_lm_year <- lm(jdate ~ year, data = bud)
+lrtest(bud_lm_year, bud_model)
+# Flower
+flower_lm_year <- lm(jdate ~ year, data = flower)
+lrtest(flower_lm_year, flower_model)
+# Fruit
+fruit_lm_year <- lm(jdate ~ year, data = fruit)
+lrtest(fruit_lm_year, fruit_model)
+# Seed disperse
+sd_lm_year <- lm(jdate ~ year, data = sd)
+lrtest(sd_lm_year, sd_model)
 
